3e1ed15cfcfdc8c25325e55dd95bbc89cc58b2f4
Przemyslaw Zawadzki <zawadzki1994@o2.pl>
2023-02-14 23:12:19 +0100

    Changed README.md, Create new project 'ASME Calculator'

---
+++ ASME BPVC Sec. VIII Div. 1 Calculator/ASME BPVC Sec. VIII Div. 1 Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace ASME_BPVC_Sec._VIII_Div._1_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("This is simply calculator for pressure parts acc. to ASME BPVC Sec. VIII Div. 1");
+            Console.WriteLine("\nwhat do you want to calculate?");
+            Console.WriteLine("\nPress 1 for cylindrical shell");
+            Console.WriteLine("\nPress 0 to exit");
+
+            int action = Convert.ToInt32(Console.ReadLine());
+
+
+            switch (action) 
+            {
+                case 1:
+                {
+                        Console.WriteLine("Enter shell outside diameter D_ext [mm]");
+                        int shellOutsideDiameter = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"D_ext = {shellOutsideDiameter}");
+                        break;
+                }
+
+                case 0:
+                {
+                    Console.WriteLine("See you!");
+                    break;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ASME-VIII-calculator
+
+**Simply calculator for pressure elements acc. to ASME BPVC Sec. VIII Div.1**
+
+##CONTENTS
+
+1. Cylindrical shell
